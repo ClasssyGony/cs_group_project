06c6916ebb5cae14a00defe7f703ddf572adb0d5
ClasssyGony <classsygony@gmail.com>
2026-01-09 10:20:13 +0000

    Added testing of button to main along with button update module

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
+from support.button import Button
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+#Test Button
+button1 = Button(screen, 100,100, (255,255,255))
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+    #Update button
+    button1.update(screen)
+
     clock.tick(120)
+    
+    #Refrest the screen every tick
+    pygame.display.update()
 
 pygame.quit()
--- support/button.py
+++ support/button.py
@@ -4,8 +4,9 @@ class Button:
     def __init__(self,surface, width, height, colour):
         self.width = width
         self.height = height
+        self.colour = colour
 
-        self.button = pygame.draw.rect(surface,colour,(self.width, self.height))
+        self.button = pygame.draw.rect(surface,colour,(500,500,self.width, self.height))
 
     def update(self,surface):
-        surface.blit(self.button)
+        self.button = pygame.draw.rect(surface,self.colour,(500,500,self.width, self.height))
