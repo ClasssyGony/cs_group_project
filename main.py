9fbb460b1acf98f097ec27faf3f5ead2cc8a745e
13Sam24 <richesam12@gmail.com>
2026-01-14 10:00:20 +0000

    Adding the Random word picking and checking inputs
    There is currently no way to input text but you will have to use :
    
    codeword, correct = checkInput(userInput, chosenWord, codeWord)
    
    the code word is updates to have letters in the correctly guessed spaces and the correct is a true and falce depending on weather the input was included at all.

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
+import random
 import pygame
 from support.button import Button
 
 # Makeing the word list
 wordList = []
 with open("wordlist.txt", 'r') as file:
-    input = file.read().splitlines()
+    wordList = file.read().splitlines()
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
 #Test Button
 button1 = Button(screen, 100,100, (255,255,255))
 
+#pick random word
+def pickWord(wordList):
+    chosenWord = random.choice(wordList)
+    codedWord = ""
+    for i in range(len(chosenWord)):
+        codedWord = codedWord + "_"
+    return codedWord, chosenWord
+
+# stuff for text
+display_surface = pygame.display.set_mode((1280, 730))
+codedWord, chosenWord = pickWord(wordList)
+font = pygame.font.Font("freesansbold.ttf", 32)
+black = 0, 0, 0
+white = 255, 255, 255
+displayedCodedWord = font.render(codedWord, True, black, white)
+codedWordRect = displayedCodedWord.get_rect()
+codedWordRect.center = (640, 300)
+
+#showing the real word to make it easier
+displayRealWord = font.render(chosenWord, True, black, white)
+displayReadWordRect = displayRealWord.get_rect()
+displayReadWordRect.center = (30, 30)
+
+def checkInput(userInput, chosenWord, codedWord):
+    correct = False
+    for i in range(len(chosenWord)):
+        if userInput == chosenWord[i]:
+            temp = codedWord[:i] + userInput + codedWord[i+1:]
+            codedWord = temp
+            correct = True
+    
+    return codedWord, correct
+
+codedWord, chosenWord = pickWord(wordList)
+
+# codedWord, correct = checkInput(userInput, chosenWord, codedWord)
+
 while running:
+    # showing the coded word
+    display_surface.fill(white)
+    display_surface.blit(displayedCodedWord, codedWordRect)
+
+    # showing the real word
+    display_surface.blit(displayRealWord)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
---
+++ temp.py
@@ -0,0 +1,32 @@
+import random
+
+wordList = []
+with open("wordlist.txt", 'r') as file:
+    wordList = file.read().splitlines()
+
+
+def pickWord(wordList):
+    chosenWord = random.choice(wordList)
+    codedWord = ""
+    for i in range(len(chosenWord)):
+        codedWord = codedWord + "_"
+    return codedWord, chosenWord
+
+def checkInput(userInput, chosenWord, codedWord):
+    correct = False
+    for i in range(len(chosenWord)):
+        if userInput == chosenWord[i]:
+            temp = codedWord[:i] + userInput + codedWord[i+1:]
+            codedWord = temp
+            correct = True
+    
+    return codedWord, correct
+
+codedWord, chosenWord = pickWord(wordList)
+
+print(codedWord, chosenWord)
+userInput = input()
+
+codedWord, correct = checkInput(userInput, chosenWord, codedWord)
+
+print(f"the coded word is\n{codedWord}\nand you got that letter {correct}")
