e8dafc6cd784de2d1135a531d9d2ab50e1a7fd7b
Joel <joelkneller7@gmail.com>
2026-02-03 13:06:32 +0000

    Modify word selection logic based on reputation

--- support/managingWord.py
+++ support/managingWord.py
@@ -9,15 +9,30 @@ with open("wordlist.txt", 'r') as file:
     for word in words:
         wordList.append(word.lower())
 
-    
-
+#used in testing
+reputation = 5
 
 #picking the word
 def pickWord(wordList):
     global codedWord
     chosenWord = random.choice(wordList)
+    #how reputation affects the game
+    if reputation <= 25:
+        while len(chosenWord) >= 7:
+            chosenWord = random.choice(wordList)
+    elif reputation <= 75 and reputation > 25:
+        while len(chosenWord) > 10 or len(chosenWord) < 6:
+            chosenWord = random.choice(wordList)
+    elif reputation <= 150 and reputation > 75:
+        while len(chosenWord) > 12 or len(chosenWord) < 8:
+            chosenWord = random.choice(wordList)
+    elif reputation <= 250 and reputation > 150:
+        while len(chosenWord) >= 15 or len(chosenWord) < 10:
+            chosenWord = random.choice(wordList)
+    elif reputation > 250:
+        while len(chosenWord) <= 12:
+            chosenWord = random.choice(wordList)
     codedWord = ""
-   # print(chosenWord)
     for i in range(len(chosenWord)):
         codedWord = codedWord + "_"
     return codedWord, chosenWord
@@ -62,7 +77,7 @@ def displayWord(screen,font,chosenWord):
 
     screen.fill((255, 255, 255))
     screen.blit(displayedCodedWord, codedWordRect)
-    #screen.blit(displayRealWord)
+    screen.blit(displayRealWord)
 
 def getMouseClick(pressed):
     if pygame.mouse.get_pressed()[0] and pressed == False:
